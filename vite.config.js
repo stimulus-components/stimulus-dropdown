5d0770b3948a09de5b2562cd9ea9d6b958cad6a1
Guillaume Briday <guillaumebriday@gmail.com>
2022-12-23 18:57:38 -0500

    Bump to Vite 4 and adding lib name

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import path from 'path'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default ({ mode }) => {
-  if (mode === 'netlify') return {}
+export default defineConfig(({ mode }) => {
+  if (mode === 'netlify') {
+    return {}
+  }
 
   return {
     build: {
       lib: {
-        entry: path.resolve(__dirname, 'src/index.ts'),
-        name: 'stimulus-dropdown'
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'StimulusDropdown',
+        fileName: 'stimulus-dropdown'
       },
       rollupOptions: {
         external: ['stimulus-use', '@hotwired/stimulus'],
@@ -20,4 +24,4 @@ export default ({ mode }) => {
       }
     }
   }
-}
+})
