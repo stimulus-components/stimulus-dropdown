bfa52c2d798348852f14c5ec615d1701b7141610
AxelBriche <axel.briche.pro@gmail.com>
2023-03-21 11:42:01 +0100

    prevent dropdown menu outside the screen

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,51 @@ export default class extends Controller {
     })
   }
 
-  toggle (): void {
+  toggle ({
+    target
+  }: {
+    target: HTMLInputElement
+  }): void {
     this.toggleTransition()
+    this.updateMenuPosition(target.offsetHeight)
+  }
+
+  updateMenuPosition (menuOffsetHeight: number): void {
+    const menuRect = this.menuTarget.getBoundingClientRect()
+
+    if (menuRect.left < 0) {
+      this.hangLeft()
+    }
+    else if (menuRect.right > window.innerWidth) {
+      this.hangRight()
+    }
+
+    if (menuRect.top < 0) {
+      this.hangTop(menuOffsetHeight)
+    }
+    else if (menuRect.bottom > window.innerHeight) {
+      this.hangBottom(menuOffsetHeight)
+    }
+  }
+
+  hangLeft (): void {
+    this.menuTarget.classList.add('left-0')
+    this.menuTarget.classList.remove('right-0')
+  }
+  hangRight (): void {
+    this.menuTarget.classList.add('right-0')
+    this.menuTarget.classList.remove('left-0')
+  }
+  hangBottom (buttonHeight: number): void {
+    this.menuTarget.style.bottom = `${buttonHeight + 10}px`;
+  }
+  hangTop (buttonHeight: number): void {
+    this.menuTarget.style.top = `${buttonHeight + 10}px`;
   }
 
   hide (event: Event): void {
-    // @ts-ignore
-    if (!this.element.contains(event.target) && !this.menuTarget.classList.contains('hidden')) {
+    const node = event.target as Node
+    if (!this.element.contains(node) && !this.menuTarget.classList.contains('hidden')) {
       this.leave()
     }
   }
